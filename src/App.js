11ed660e3206056a4ea5576536b40f07cb738d33
Michael Carr <mcarr2468@gmail.com>
2021-10-14 18:13:59 -0500

    V1.0 starter html & css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Pagination</h1>
+
+      <section className="card-section">
+        <ul className="cards">
+          <li className="card">
+            <img className="card__image" src="https://avatars.githubusercontent.com/u/35147?v=4" alt=""/>
+            <h2 className="card__name">steve</h2>
+            <button className="card__view-profile-btn"><a href="/">View Profile</a></button>
+          </li>
+
+          <li className="card">
+            <img className="card__image" src="https://avatars.githubusercontent.com/u/35147?v=4" alt=""/>
+            <h2 className="card__name">bob</h2>
+            <button className="card__view-profile-btn"><a href="/">View Profile</a></button>
+          </li>
+
+          <li className="card">
+            <img className="card__image" src="https://avatars.githubusercontent.com/u/35147?v=4" alt=""/>
+            <h2 className="card__name">steve</h2>
+            <button className="card__view-profile-btn"><a href="/">View Profile</a></button>
+          </li>
+        </ul>
+      </section>
+
+      <section className="pages-section">
+        <button className="pages-section__prev">Prev</button>
+        <ul className="pages">
+          <li className="page">
+            <button className="page__btn">1</button>
+          </li>
+          <li className="page">
+            <button className="page__btn">2</button>
+          </li>
+          <li className="page">
+            <button className="page__btn">3</button>
+          </li>
+          <li className="page">
+            <button className="page__btn">1</button>
+          </li>
+          <li className="page">
+            <button className="page__btn">2</button>
+          </li>
+          <li className="page">
+            <button className="page__btn">3</button>
+          </li>
+          <li className="page">
+            <button className="page__btn">1</button>
+          </li>
+          <li className="page">
+            <button className="page__btn page__btn--active">2</button>
+          </li>
+          <li className="page">
+            <button className="page__btn">3</button>
+          </li>
+        </ul>
+        <button className="pages-section__next">Next</button>
+      </section>
+
     </div>
   );
 }
